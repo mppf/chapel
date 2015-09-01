a1524a997c3848634d1c3e861cdb9d907d4dfa9f
Ray Chen <rchen@cs.umd.edu>
2015-09-01 05:57:01 -0400

    Add flag to print valid options for CHPL_TUNER.

--- util/chplenv/chpl_tuner.py
+++ util/chplenv/chpl_tuner.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
-import os
+import os, optparse
 from sys import stdout, stderr
 from utils import memoize
 
+valid_tuners = set(['none', 'harmony'])
+
 @memoize
 def get():
-    tuner_val = os.environ.get('CHPL_TUNER')
-    if not tuner_val:
-        tuner_val = 'none'
-    elif tuner_val not in ('none', 'harmony'):
-        stderr.write('Warning: Unexpected tuner value {0}\n'.format(tuner_val))
-        tuner_val = ''
-    return tuner_val
+    TUNER_val = os.environ.get('CHPL_TUNER')
+    if not TUNER_val:
+        TUNER_val = 'none'
+    elif TUNER_val not in valid_tuners:
+        stderr.write('Warning: Invalid tuner value "{0}"\n'.format(TUNER_val))
+        TUNER_val = ''
+    return TUNER_val
 
 def _main():
-    tuner_val = get()
-    stdout.write("{0}\n".format(tuner_val))
+    parser = optparse.OptionParser()
+    parser.add_option('-l', '--list', dest='list', action='store_true',
+                      default=False, help='display the list of valid tuners')
+    (options, args) = parser.parse_args()
+
+    if options.list:
+        stdout.write("Valid tuners: {0}\n".format(", ".join(valid_tuners)))
+    else:
+        TUNER_val = get()
+        stdout.write("{0}\n".format(TUNER_val))
 
 
 if __name__ == '__main__':
