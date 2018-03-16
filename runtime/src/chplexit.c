9d77ce8bca60c12f30b4ff3b96c75243655ea651
Michael Ferguson <mppf@users.noreply.github.com>
2018-03-16 09:49:27 -0400

    Clean up QSBR after task layer exits
    Otherwise there might be a use-after-free - we don't
    know there's only 1 thread running until after
    chpl_task_exit.
    
    To see this issue, run:
    
     ./util/start_test  -valgrindexe test/distributions/robust/arithmetic/basics/test_zipper_default.chpl
    
    in a quickstart configuration.

--- runtime/src/chplexit.c
+++ runtime/src/chplexit.c
@@ -40,8 +40,8 @@ static void chpl_exit_common(int status, int all) {
   }
   chpl_comm_pre_task_exit(all);
   if (all) {
-    chpl_qsbr_exit();
     chpl_task_exit();
+    chpl_qsbr_exit();
     chpl_reportMemInfo();
   }
   chpl_comm_exit(all, status);
